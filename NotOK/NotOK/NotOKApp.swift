7e0c3081e66819f6b0a5faca208713552a31b7b4
Tan Xin Jie <xjtan99@gmail.com>
2025-03-04 17:07:09 +0800

    firebase init

--- NotOK/NotOK/NotOKApp.swift
+++ NotOK/NotOK/NotOKApp.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 
 @main
 struct NotOKApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
